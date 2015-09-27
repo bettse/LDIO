3344562c7ae1e459126bd3f27c262e8c5245626b
Eric Betts <bettse@fastmail.fm>
2015-09-27 08:32:12 -0700

    Initial Commit

---
+++ LDIO/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LDIO
+//
+//  Created by Eric Betts on 9/27/15.
+//  Copyright © 2015 Eric Betts. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LDIO/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LDIO
+//
+//  Created by Eric Betts on 9/27/15.
+//  Copyright © 2015 Eric Betts. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
