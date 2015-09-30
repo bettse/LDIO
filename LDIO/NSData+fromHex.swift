06658242244f9f934d6a0463f73882bc637df6f5
Eric Betts <bettse@fastmail.fm>
2015-09-30 00:00:07 -0700

    Change from string->nsdata to nsdata(fromhex:)

---
+++ LDIO/NSData+fromHex.swift
@@ -0,0 +1,31 @@
+//
+//  String+hex.swift
+//  LDIO
+//
+//  Created by Eric Betts on 9/29/15.
+//  Copyright © 2015 Eric Betts. All rights reserved.
+//
+
+import Foundation
+
+public extension NSData {
+    convenience init(fromHex: String) {
+        
+        let hexArray = fromHex.componentsSeparatedByString(" ")
+        let hexBytes : [UInt8] = hexArray.map({UInt8($0, radix: 0x10)!})
+        self.init(bytes: hexBytes as [UInt8], length: hexBytes.count)
+        
+    }
+    
+    subscript(origin: Int) -> UnsafePointer<UInt8> {
+        get {
+            var result: UnsafePointer<UInt8> = nil;
+            if (origin < self.length) {
+                let newLength = self.length - origin;
+                let tempData = self.subdataWithRange(NSMakeRange(origin, newLength));
+                result = UnsafePointer<UInt8>(tempData.bytes);
+            }
+            return result;
+        }
+    }
+}
