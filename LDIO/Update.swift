2adbaff3f1a2666eddb08dbb9c8d7d29cbf2099d
Eric Betts <bettse@fastmail.fm>
2015-12-09 20:33:10 -0800

    Add SAK to Update

--- LDIO/Update.swift
+++ LDIO/Update.swift
@@ -12,6 +12,7 @@ import Foundation
 class Update : Message {
     
     let ledPlatformIndex = 0
+    let sakIndex = 1
     let nfcIndexIndex = 2
     let directionIndex = 3
     let tagUidIndex = 4
@@ -24,23 +25,40 @@ class Update : Message {
         }
     }
 
+    //http://nfc-tools.org/index.php?title=ISO14443A
+    enum Sak : UInt8 {
+        case MifareUltralight = 0x00
+        case MifareClassic1k = 0x08
+        case MifareMini = 0x09
+        case MifareClassic4k = 0x18
+        case MifareDesFire = 0x20
+        case Unknown = 0xFF //Not standard
+    }
     
     //Setting defaults so I don't have to deal with '?' style variables yet
     var ledPlatform : LedPlatform = .None
     var nfcIndex : UInt8 = 0
+    var sak : Sak = .Unknown
     var direction : Direction = .Arriving
     var uid : NSData
     
     init(data: NSData) {
-        data.getBytes(&ledPlatform, range: NSMakeRange(ledPlatformIndex, sizeof(LedPlatform)))
-        data.getBytes(&nfcIndex, range: NSMakeRange(nfcIndexIndex, sizeof(nfcIndex.dynamicType)))
-        data.getBytes(&direction, range: NSMakeRange(directionIndex, sizeof(Direction)))
+        if let ledPlatform = LedPlatform(rawValue: data[ledPlatformIndex]) {
+            self.ledPlatform = ledPlatform
+        }
+        if let sak = Sak(rawValue: data[sakIndex]) {
+            self.sak = sak
+        }
+        nfcIndex = data[nfcIndexIndex]
+        if let direction = Direction(rawValue: data[directionIndex]) {
+            self.direction = direction
+        }
         uid = data.subdataWithRange(NSMakeRange(tagUidIndex, NTAG213.uidLength))
     }
     
     
     override var description: String {
         let me = String(self.dynamicType).componentsSeparatedByString(".").last!
-        return "\(me)(\(ledPlatform.desc()) \(nfcIndex) \(direction.desc()) \(uid.hexadecimalString()))"
+        return "\(me)(\(ledPlatform.desc()) SAK:\(sak) \(nfcIndex) \(direction.desc()) \(uid.hexadecimalString()))"
     }
 }
